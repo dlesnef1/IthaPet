2a93a4f3aa2781cc602b848c2967de63473db65f
David <davidlesnefsky@gmail.com>
2015-12-07 18:29:09 -0500

    Rails initialization

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  resources :users
+end
