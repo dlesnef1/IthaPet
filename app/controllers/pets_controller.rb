740e2dace82379ec379df70d7fc976c608655c38
David <davidlesnefsky@gmail.com>
2015-12-10 13:26:06 -0500

    Updating pets better

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,4 +1,9 @@
 class PetsController < ApplicationController
+    include PetsHelper
+
+    before_action :logged_in_user, only: [:edit]
+    before_action :correct_user,   only: [:edit]
+
     def index
         @pets = Pet.paginate(page: params[:page])
     end
@@ -7,19 +12,67 @@ class PetsController < ApplicationController
         @pet = Pet.find(params[:id])
     end
 
-
     def update
         @pet = Pet.find(params[:id])
-        if @pet.update_attributes(pet_params)
+        if params[:adopt] == "TRUE"
+            @pet.update(user: current_user)
+            render "show"
+        elsif !params[:adopt].empty?
+            message = interact(@pet, params[:adopt])
+
+            if message != "nothing"
+                flash[:danger] = message
+                redirect_to '/'
+                return
+            end
+
+            render "edit"
+        else
+            flash[:danger] = "Something went wrong!"
+            redirect_to '/'
+        end
+    end
+
+    def edit
+        @pet = Pet.find(params[:id])
+    end
+
+    # Just for us to easily add pets
+    def new
+        @pet = Pet.new
+    end
+
+    def create
+        @pet = Pet.new(pet_params)
+        @pet.update(hungriness: 0, happiness: 100, cleanliness: 50, loyalty: 50)
+        if @pet.save
+            flash[:success] = "Added pet successfully!"
             redirect_to @pet
         else
-            render 'edit'
+        render 'new'
         end
     end
 
     private
 
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+        @pet = Pet.find(params[:id])
+        unless current_user == @pet.user
+            flash[:danger] = "Not your pet!"
+            redirect_to login_url
+        end
+    end
+
     def pet_params
-      params.require(:pet).permit(:name, :image, :user)
+      params.require(:pet).permit(:name, :biography, :image)
     end
 end
--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,2 +1,44 @@
 module PetsHelper
+    def howHappy(pet)
+        if pet.happiness > 75
+            "http://emojipedia-us.s3.amazonaws.com/cache/9f/7d/9f7df7b8aaad4ac1e897259a29995f3a.png"
+        elsif pet.happiness > 25
+            "http://emojipedia-us.s3.amazonaws.com/cache/45/99/4599951f963380afc847d357adc68ace.png"
+        else
+            "http://emojipedia-us.s3.amazonaws.com/cache/6d/5a/6d5a25b6b679412b8211a9f717d81366.png"
+        end
+    end
+
+    def interact(pet, command)
+        if command == "Feed"
+            pet.update(hungriness: pet.hungriness-10,
+                happiness: pet.happiness+5, cleanliness: pet.cleanliness-5, loyalty: pet.loyalty+5)
+        elsif command == "Wash"
+            pet.update(happiness: pet.happiness-5, cleanliness: pet.cleanliness+10, loyalty: pet.loyalty-5)
+        elsif command == "Walk"
+            pet.update(hungriness: pet.hungriness+5,
+                happiness: pet.happiness+10, cleanliness: pet.cleanliness-5, loyalty: pet.loyalty+5)
+        elsif command == "Pet"
+            pet.update(happiness: pet.happiness+1, loyalty: pet.loyalty+1)
+        end
+        checkSums(pet)
+    end
+
+    def checkSums(pet)
+        attributes = [:hungriness, :happiness, :loyalty, :cleanliness]
+        attributes.each do |attribute|
+            if pet[attribute] > 100
+                pet[attribute] = 100
+            elsif pet[attribute] < 0
+                pet[attribute] = 0
+            end
+        end
+
+        # bugs, but will be fixed soon
+        if pet.hungriness > 80
+            pet.update(user: nil, hungriness: 0, cleanliness: 100, happiness: 25, loyalty: 10)
+            return "PETA would like a word with you! #{pet.name} is going to a better home!"
+        end
+        return "nothing"
+    end
 end
